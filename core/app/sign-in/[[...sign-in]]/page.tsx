8afb54de2d28028cff3e098dec4713f2befd31b3
archangel-12 <alexandermatthew25@gmail.com>
2025-06-17 14:13:07 +0000

    refactor sign-in page layout and update image display although it did not succeed

--- core/app/sign-in/[[...sign-in]]/page.tsx
+++ core/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,21 +1,23 @@
 import { SignIn } from "@clerk/nextjs";
+import images from "next/image";
 
 export default function Page() {
   return (
-    <div className="h-screen overflow-hidden grid lg:grid-cols-2">
+    <div className="overflow-hidden grid lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            Velvet
-          </a>
-        </div>
+        <div className="/">
+            <img
+              src="https://i.pinimg.com/originals/28/28/38/28283832b82999acea2c337704c5d9ed.gif"
+              alt="Image"
+            />
+          </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             {/* You can add additional content here if needed */}
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center p-4 overflow-y-auto">
+      <div className="flex items-center justify-center p-1 overflow-y-auto">
         <div className="w-full max-w-md">
           <SignIn
             afterSignInUrl="/chat"
